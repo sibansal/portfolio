ba603ad8fb43f38d7bf22bb1a78d340dd63bc795
Simran Singh Bansal <36766112+sibansal@users.noreply.github.com>
2022-05-10 02:29:20 +0530

    Styles updated...

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ class Contact extends Component
         return (
         <div className="wrapper">
             <div style={{background:"black",width:"70vw", height:"60vh", border:"1vw solid black"}}>
-                <iframe scrolling="no" frameBorder="0" src="https://tawk.to/chat/6276e858b0d10b6f3e7119cd/1g2g7n7op" width="100%" height="100%"/>
+                <iframe title="contact" scrolling="no" frameBorder="0" src="https://tawk.to/chat/6276e858b0d10b6f3e7119cd/1g2g7n7op" width="100%" height="100%"/>
             </div>
         </div>
         )

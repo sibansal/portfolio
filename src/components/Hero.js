0d105564677d8786a6f7b1dfc86f7d3ab9944484
Simran Singh Bansal <36766112+sibansal@users.noreply.github.com>
2022-05-09 13:22:18 +0530

    Values updated...

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,12 +7,12 @@ class Hero extends Component
         return (
         <div className="hero wrapper">
             <img alt="dp" src="./img/dp.jpg" className="dp" />
-            <h1 className="title">I'm Simran Singh Bansal</h1>
-            <h2 className="description">A web developer</h2>
+            <h1 className="title">I'm Simran Bansal</h1>
+            <h2 className="description">A Software Engineer</h2>
             <div>
                 <div className="social">
-                    <span><a href="https://www.linkedin.com/in/simransinghbansal/" className="social-link"><i className="fab fa-linkedin"></i></a></span>
-                    <span><a href="https://www.github.com/simransinghbansal" className="social-link"><i className="fab fa-github"></i></a></span>
+                    <span><a href="https://www.linkedin.com/in/sibansal/" className="social-link"><i className="fab fa-linkedin"></i></a></span>
+                    <span><a href="https://www.github.com/sibansal" className="social-link"><i className="fab fa-github"></i></a></span>
                 </div>
             </div>
         </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,10 +11,10 @@ class Navbar extends Component
                     <li><NavLink to="/" className="link">Home</NavLink></li>
                     <li><NavLink to="/skills" className="link">Skills</NavLink></li>
                     <li><NavLink to="/projects" className="link">Projects</NavLink></li>
-                    <li>
+                    {/* <li>
                         <a target="_new"
                         href="https://tawk.to/chat/6276e858b0d10b6f3e7119cd/1g2g7n7op" className="link">Contact</a>
-                    </li>
+                    </li> */}
                 </ul>
             </nav>
         )

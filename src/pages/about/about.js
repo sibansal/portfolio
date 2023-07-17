686eeb1e3d9891412cb57382b1e19757464bca1c
Simran Bansal <contributions@sibansal.dev>
2023-07-17 11:50:27 +0530

    About page reverted back...

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,42 +1,53 @@
-import React, { Component } from 'react'
+import React,{ Component } from 'react'
 import './about.css'
-class About extends Component {
-  render() {
-    return (
-      <div className='layout about'>
-        <h2>About Me</h2>
-        <div className='content'>
-          <p>
-            <span role="img" aria-label="Waving hand">👋</span> Hey there! Since my childhood, I've had an unyielding passion for technology. It's been the driving force behind my pursuit of technical education. Over time, my thirst for knowledge and growth in this field has only intensified. <span role="img" aria-label="Sparkles">🌟</span> However, I've come to realize that technology isn't just about me; it's about all of us working together as a team. <span role="img" aria-label="Handshake">🤝</span> A well-functioning system involves hundreds and thousands of components coming together to achieve a common goal. If I am 1, then a team of two is 11, and with three, we become 111! Together, we can accomplish great things. <span role="img" aria-label="Rocket">🚀</span>
-          </p><br />
-
-          <p>
-            When it comes to my projects, I firmly believe in expression rather than merely leaving an impression. That's why I enjoy building them from scratch, which has resulted in a current success rate of 70%. <span role="img" aria-label="Building Construction">🏗️</span><span role="img" aria-label="Flexed Biceps">💪</span>
-          </p><br />
-
-          <p>
-            Now, let's dive into some of the undocumented skills I possess that you might find interesting. Here they are: event coordination, graphic design, project planning, strategic planning, and teaching. <span role="img" aria-label="Artist Palette">🎨</span><span role="img" aria-label="Calendar">📆</span><span role="img" aria-label="Graduation Cap">🎓</span> These hidden talents often surprise and add value to those I collaborate with.
-          </p><br />
-
-          <p>
-            In addition, I'm proficient in the following documented skills: software development and web development. <span role="img" aria-label="Desktop Computer">🖥️</span><span role="img" aria-label="Globe with Meridians">🌐</span> These areas have been my trusted allies throughout my journey, enabling me to deliver top-notch results.
-          </p><br />
-
-          <p>
-            Of course, like everyone, I have my areas of improvement. I must confess that badminton, dancing, drawing, singing, and writing are not my strong suits. <span role="img" aria-label="Badminton">🏸</span><span role="img" aria-label="Woman Dancing">💃</span><span role="img" aria-label="Writing Hand">✍️</span> But hey, life would be dull if we were good at everything, right? <span role="img" aria-label="Grinning Face with Smiling Eyes">😄</span>
-          </p><br />
-
-          <p>
-            And finally, there are some things that simply aren't meant for me. You won't find me boasting about good handwriting, delivering long lectures, or excelling at outdoor sports. <span role="img" aria-label="Memo">📝</span><span role="img" aria-label="Speaking Head">🗣️</span><span role="img" aria-label="National Park">🏞️</span> We all have our unique talents and preferences!
-          </p><br />
-
-          <p>
-            I want to extend my sincere appreciation for your patience and for taking the time to visit my website. Your valuable time investment means a lot to me! If you have any questions or would like to explore potential collaborations, please feel free to reach out. Let's make magic happen! <span role="img" aria-label="Sparkles">✨</span><span role="img" aria-label="Raising Hands">🙌</span>
-          </p>
-        </div>
-
-      </div>);
-  }
+class About extends Component
+{
+    render()
+    {
+        return (
+            <div className='layout about'>
+                <h2>About Me</h2>
+                <div className='content'>
+                    <div>
+                        <p>
+                        Since my childhood, I've had a deep passion for technology which drove me to pursue technical education. My thirst for knowledge and growth in this field has only intensified over time. However, I've come to realize that technology is not just about me, but about us. A good working system involves hundreds and thousands of components working together as a team to achieve a common goal. If I am 1, then a team of two is 11, and three is 111. Together, we can accomplish great things.
+                        </p><br />
+                        <p>
+                        When it comes to my projects, I believe in expression rather than just leaving an impression. That's why I often build them from scratch, resulting in a 70% success rate as of now.
+                        </p><br />
+                        Here are some of the undocumented skills I possess that you might find interesting:<br />
+                        <ul>
+                            <li>Event coordination</li>
+                            <li>Graphic design</li>
+                            <li>Project planning</li>
+                            <li>Strategic Planning</li>
+                            <li>Teaching</li>
+                        </ul><br/>
+                        I'm also proficient in the following documented skills:<br />
+                        <ul>
+                            <li>Software development</li>
+                            <li>Web development</li>
+                        </ul><br/>
+                        Now, let's talk about some of the things I'm not so good at:<br />
+                        <ul>
+                            <li>Badminton</li>
+                            <li>Dancing</li>
+                            <li>Drawing</li>
+                            <li>Singing</li>
+                            <li>Writing</li>
+                        </ul><br/>
+                        Finally, there are some things that are just not meant for me:<br/>
+                        <ul>
+                            <li>Good handwriting</li>
+                            <li>Long lectures</li>
+                            <li>Outdoor sports</li>
+                        </ul><br/>
+                        I appreciate your patience and valuable time investment in visiting my website. Please feel free to reach out to me if you have any questions or would like to discuss potential collaborations.
+                    </div>
+                </div>
+            </div>
+          )
+    }
 }
 
 export default About;

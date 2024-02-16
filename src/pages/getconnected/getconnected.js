c54f5dda214fefc320a733b4ea323f771264d60a
Simran Bansal <contributions@sibansal.dev>
2024-02-16 22:03:47 +0530

    React icons added...

--- src/pages/getconnected/getconnected.js
+++ src/pages/getconnected/getconnected.js
@@ -1,5 +1,6 @@
 import React,{ Component } from 'react'
 import './getconnected.css'
+import { TiSocialInstagramCircular } from "react-icons/ti";
 
 const callImg = '/svg/call.svg'
 const githubImg = '/svg/github.svg'
@@ -17,10 +18,11 @@ class GetConnected extends Component
                 <div className='content'>
                     <div><a href='https://topmate.io/sibansal'><img src={callImg} className='align-middle' alt={'Book Free 1:1 Call'}/> Book 1:1 Call</a></div>
                     <div><a href='https://t.me/sibansal'><img src={telegramImg} className='align-middle' alt={'Telegram'}/> t.me/sibansal</a></div>
-                    <div><a href='https://twitter.com/sibansal'><img src={twitterImg} className='align-middle' alt={'Twitter'}/> twitter.com/sibansal</a></div>
+                    <div><a href='https://twitter.com/sibansal'><img src={twitterImg} className='align-middle' alt={'Twitter'}/> @sibansal</a></div>
+                    <div><a href='https://instagram.com/isimranbansal'><TiSocialInstagramCircular size={32} className='align-middle' />@isimranbansal</a></div>
                     <div><a href='https://github.com/sibansal'><img src={githubImg} className='align-middle' alt={'Github'}/> github.com/sibansal</a></div>
-                    <div><a href='mailto:queries@sibansal.dev'><img src={mailImg} className='align-middle' alt={'Mail'}/> queries@sibansal.dev</a></div>
                     <div><a href='mailto:hireme@sibansal.dev'><img src={mailImg} className='align-middle' alt={'Mail'}/> hireme@sibansal.dev</a></div>
+                    <div><a href='mailto:queries@sibansal.dev'><img src={mailImg} className='align-middle' alt={'Mail'}/> queries@sibansal.dev</a></div>
                 </div>
             </div>
           )
